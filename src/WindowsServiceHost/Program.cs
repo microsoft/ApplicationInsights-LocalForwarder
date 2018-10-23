4ac244ee951411eb3718f08659046c6fae582d8d
Alexander Kaplan <alkaplan@microsoft.com>
2018-10-23 15:07:04 -0700

    Removing garbage.

--- src/WindowsServiceHost/Program.cs
+++ src/WindowsServiceHost/Program.cs
@@ -1,30 +1,16 @@
 ﻿namespace Microsoft.LocalForwarder.WindowsServiceHost
 {
     using System;
-    using System.Collections.Generic;
     using System.Diagnostics;
-    using System.Reflection;
     using System.ServiceProcess;
-    using System.Xml.Linq;
 
     static class Program
     {
-        static void LoadInstances1<T>(XElement definition, ICollection<T> instances, int modules)
-        {
-            int a = 1 + 2;
-            return;
-        }
-
         /// <summary>
         /// The main entry point for the application.
         /// </summary>
         static void Main()
         {
-            foreach (var m in typeof(Program).GetRuntimeMethods())
-            {
-                Console.WriteLine(m.Name);
-            }
-
             if (Environment.UserInteractive && Debugger.IsAttached)
             {
                 // debugging
@@ -37,7 +23,7 @@
                 ServiceBase[] ServicesToRun;
                 ServicesToRun = new ServiceBase[]
                 {
-                new LocalForwarderHostService()
+                    new LocalForwarderHostService()
                 };
                 ServiceBase.Run(ServicesToRun);
             }
