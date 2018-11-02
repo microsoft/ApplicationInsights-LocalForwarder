bbca2782126cba4d0b22e257949e4a02d2f4fa6b
David Duffy <daduffy@microsoft.com>
2018-11-02 16:27:39 -0700

    Completed example NodeJS app using opencensus with AppInsights local forwarder.

--- examples/opencensus/node-app/app.js
+++
@@ -1,8 +0,0 @@
-const http=require('http');
-
-console.log("Starting.");
-http.createServer((req, res) => {
-    res.end("Hello Earth!");
-    console.log("We got a call! :)");
-}).listen(8008);
-console.log("Started.");
